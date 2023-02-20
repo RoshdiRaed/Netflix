a0d88af508278b45d1772e8239a816f2c1ed88fa
Antonio <hedhogs@gmail.com>
2023-02-20 05:25:31 +0100

    ci: fix

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -2,10 +2,12 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma;
