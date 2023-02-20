31e47d5736751e97339fbac082e24f81e9b8bd6d
Antonio <hedhogs@gmail.com>
2023-02-20 05:00:57 +0100

    fix(lint): escape apostrophe

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -57,7 +57,7 @@ const App = () => {
   return (
     <div className="flex items-center h-full justify-center">
       <div className="flex flex-col">
-        <h1 className="text-6xl text-white text-center">Who's watching?</h1>
+        <h1 className="text-6xl text-white text-center">Who&#39;s watching?</h1>
         <div className="flex items-center justify-center gap-8 mt-10">
           <div onClick={() => selectProfile()}>
             <UserCard name={currentUser?.name} />
