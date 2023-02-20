0a76c402cb85e7b066741c1d51bcab5812c1c094
Antonio <hedhogs@gmail.com>
2023-02-20 05:44:20 +0100

    ci: fix

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-const client = global.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') global.prisma = client
+const client = global.prismadb || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') global.prismadb = client
 
 export default client
